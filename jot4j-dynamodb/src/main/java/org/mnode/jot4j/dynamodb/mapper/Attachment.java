f85e89cadd16d14cdb50912df1af9c933dfc3f2b
Ben Fortuna <bfortuna@odecee.com.au>
2021-06-06 14:30:14 +1000

    Added attachment support

--- jot4j-dynamodb/src/main/java/org/mnode/jot4j/dynamodb/mapper/Attachment.java
+++ jot4j-dynamodb/src/main/java/org/mnode/jot4j/dynamodb/mapper/Attachment.java
@@ -5,20 +5,25 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import java.nio.ByteBuffer;
+
 @AllArgsConstructor
 @Builder
 @Data
-public class CalendarCategory extends AbstractCardMapper {
+public class Attachment extends AbstractCardMapper {
 
     private final String pkPrefix;
 
     @DynamoDBAttribute(attributeName = "Uid")
     private String uid;
 
-    @DynamoDBAttribute(attributeName = "Name")
-    private String name;
+    @DynamoDBAttribute(attributeName = "MessageDigest")
+    private String messageDigest;
 
-    public CalendarCategory(String pkPrefix) {
+    @DynamoDBAttribute(attributeName = "Binary")
+    private ByteBuffer binary;
+
+    public Attachment(String pkPrefix) {
         this.pkPrefix = pkPrefix;
     }
 
@@ -31,7 +36,7 @@ public class CalendarCategory extends AbstractCardMapper {
     @Override
     @DynamoDBRangeKey(attributeName = "SK")
     public String getSK() {
-        return "CATEGORY#" + name;
+        return "ATTACH#" + messageDigest;
     }
 
     @DynamoDBIndexHashKey(attributeName = "GSI1_PK", globalSecondaryIndexName = "GSI1")
@@ -46,6 +51,6 @@ public class CalendarCategory extends AbstractCardMapper {
 
     @Override
     public String getType() {
-        return pkPrefix + "_CATEGORY";
+        return pkPrefix + "_ATTACH";
     }
 }
