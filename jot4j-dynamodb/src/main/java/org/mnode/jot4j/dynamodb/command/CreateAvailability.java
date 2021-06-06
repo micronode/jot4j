ac4663730f5b02be7430fb9e9a3a96719d5428c1
Ben Fortuna <bfortuna@odecee.com.au>
2021-06-06 14:31:34 +1000

    Added availability support

--- jot4j-dynamodb/src/main/java/org/mnode/jot4j/dynamodb/command/CreateAvailability.java
+++ jot4j-dynamodb/src/main/java/org/mnode/jot4j/dynamodb/command/CreateAvailability.java
@@ -1,9 +1,16 @@
 package org.mnode.jot4j.dynamodb.command;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import net.fortuna.ical4j.model.Property;
 import net.fortuna.ical4j.model.component.VAvailability;
+import net.fortuna.ical4j.model.property.Organizer;
+import org.mnode.jot4j.dynamodb.mapper.Availability;
 
-public class CreateAvailability extends AbstractCreateCommand<VAvailability> {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateAvailability extends AbstractCommand<VAvailability> implements CreateCommand {
 
     public CreateAvailability(AmazonDynamoDB dynamoDB) {
         super(dynamoDB);
@@ -11,7 +18,21 @@ public class CreateAvailability extends AbstractCreateCommand<VAvailability> {
 
     @Override
     public void execute(VAvailability input) {
+        List<Object> model = new ArrayList<>();
 
+        Availability availability = createAvailability(input);
+        model.add(availability);
+
+        input.getAvailable().forEach(available -> {
+
+        });
+        Organizer organizer = input.getProperty(Property.ORGANIZER);
+        if (organizer != null) {
+            model.add(createOrganizer(input, "VAVAILABILITY", input.getProperty(Property.ORGANIZER)));
+        }
+
+        DynamoDBMapper mapper = new DynamoDBMapper(dynamoDB);
+        mapper.batchSave(model.toArray());
     }
 
     @Override
